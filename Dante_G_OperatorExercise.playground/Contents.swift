a8211da2f46471eaebd6331b70f6512b97d914ce
DanteGivens <67976030+DanteGivens@users.noreply.github.com>
2020-07-13 17:21:53 -0400

    Add files via upload

---
+++ Dante_G_OperatorExercise.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var BaseValue = 0
+
+var AdditionResult = 0
+
+AdditionResult = BaseValue + 6 + 19
+// any number added or changed to the base value will result into the soulution.
+
+AdditionResult = BaseValue + 9 + 3
+// this example proves what the previous comment as true. also I do not need the label "BaseValue" but it organizes my thoughts.
+
+var SubtractionResult = 0
+
+SubtractionResult =  32 - 13
+// this shows that the subtraction result will end up as any two numbers subtracted from one another.
+SubtractionResult = 19003 - 438
+// again another confirmation.
+
+var MultiplicationResult = 0
+
+MultiplicationResult = 8 * 3
+// any two numbers multiplied by one another gives the result.
+
+MultiplicationResult = 87 * 7
+//confirmation to make sure the multiplication worked.
+
+var DivisionResult = 0
+
+DivisionResult = 30 / 5
+//any two or more numbers will result into the soultion of the division result.
+
+DivisionResult = 2000000 / 10
+// conformation for the division calculation.
